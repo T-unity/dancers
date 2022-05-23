84c9e8cf23edd41869d892cc291f73cc600a428e
T-unity <takadanaritoshi@gmail.com>
2022-05-23 20:46:01 +0900

    トップページのpathを修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'top#index'
+  root 'public/top#index'
 
   # トップコントローラー
   get :about, to: 'public/top#about', as: 'about'
