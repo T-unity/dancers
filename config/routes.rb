bb931ee8f5908a37891ee7607a0b932061d9cd76
T-unity <takadanaritoshi@gmail.com>
2022-05-28 13:12:13 +0900

    public::topコントローラを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-  get 'top/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root 'public/top#index'
-
-  # トップコントローラー。静的なコンテンツのみ。
-  get :about, to: 'public/top#about', as: 'about'
-  get :test,  to: 'public/top#test',  as: 'test'
-
-  resources :posts
+  # root 'public/top#index'
 end
