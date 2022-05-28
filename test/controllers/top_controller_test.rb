7d42339eb78d4dea097d0c801b9a3dcf0b3393af
T-unity <takadanaritoshi@gmail.com>
2022-05-28 11:55:25 +0900

    自動テストWIP

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -2,7 +2,9 @@ require "test_helper"
 
 class TopControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    # top_index_urlがGETリクエストを受け付ける事を意味する。
     get top_index_url
+    # 200OKを期待する
     assert_response :success
   end
 end
