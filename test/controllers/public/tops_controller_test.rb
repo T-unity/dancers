f503a83c2777b2501ba0a38dd8e3faf872bf093e
T-unity <takadanaritoshi@gmail.com>
2022-05-29 11:22:21 +0900

    ドキュメントルートから200OKが返却されるテストを作成

--- test/controllers/public/tops_controller_test.rb
+++ test/controllers/public/tops_controller_test.rb
@@ -2,7 +2,9 @@ require "test_helper"
 
 class Public::TopsControllerTest < ActionDispatch::IntegrationTest
   test "should get top" do
-    get public_tops_top_url
+    # get public_tops_top_url
+    # ドキュメントルートから200OKが返却される事を確認。
+    get root_path
     assert_response :success
   end
 end
