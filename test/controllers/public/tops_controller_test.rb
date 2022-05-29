949984cb11cb7efa7a02a07bd463cc6b87463b33
T-unity <takadanaritoshi@gmail.com>
2022-05-29 12:27:17 +0900

    テストを追加

--- test/controllers/public/tops_controller_test.rb
+++ test/controllers/public/tops_controller_test.rb
@@ -2,17 +2,22 @@ require "test_helper"
 
 class Public::TopsControllerTest < ActionDispatch::IntegrationTest
 
-  # Expect 200OK to be returned from the top page.
+  # ルートをチェック
   test "should get top" do
     # get public_tops_top_url
     get root_path
+    # Expect 200OK to be returned from the top page.
+    assert_response :success
+    # assert_select：特定のHTMLタグが存在するかチェック
+    # assert_select "title：タイトル内の文字列をチェック
+    assert_select "title", "Dancers"
+  end
+
+  # aboutページをチェック
+  test 'should get about' do
+    get about_path
+    # 200OKが帰ってくるか。
     assert_response :success
   end
 
-  # # Expect 200OK to be returned from the top page.
-  test 'should get about' do
-    get about_path
-    assert_response :success
-  end
-  
 end
