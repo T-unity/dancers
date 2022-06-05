41ac8549a6ba7416ea9cb454d240a2b45e5b0d10
T-unity <takadanaritoshi@gmail.com>
2022-06-05 20:38:17 +0900

    微修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     # get 'tops#about'
   # end
   root 'public/tops#top'
+  # 以下2つは同義
+  # get '/about', to: 'public/tops#about'
   get :about, to: 'public/tops#about'
 
 end
