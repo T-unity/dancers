e79d8e553cc3abbc343f786ea6585c4f2b9db31c
T-unity <takadanaritoshi@gmail.com>
2022-06-05 23:06:09 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'users/new'
-  end
+  # namespace :public do
+  #   get 'users/new'
+  # end
   # namespace :public do
     # get 'tops#about'
   # end
   root 'public/tops#top'
   # 以下2つは同義
   # get '/about', to: 'public/tops#about'
-  get :about, to: 'public/tops#about', as: 'about'
+  get :about,  to: 'public/tops#about', as: 'about'
+
+  # ユーザー登録
+  get :signup, to: 'public/users#new',  as: 'signup'
 
 end
--- test/controllers/public/users_controller_test.rb
+++ test/controllers/public/users_controller_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class Public::UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get public_users_new_url
+    # get public_users_new_url
+    get signup_path
     assert_response :success
   end
 end
