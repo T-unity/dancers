23e1a56da470b6d8ca19292748f35d93ed214cdb
T-unity <takadanaritoshi@gmail.com>
2022-06-06 23:00:30 +0900

    ユーザーモデルを作成してマイグレートを実行

---
+++ db/migrate/20220606135627_create_public_users.rb
@@ -0,0 +1,10 @@
+class CreatePublicUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :public_users do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_213950) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_06_135627) do
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "public_users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
