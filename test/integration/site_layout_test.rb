b5decb23655e4133f1e912ce8a730e2d7939d519
T-unity <takadanaritoshi@gmail.com>
2022-06-06 20:52:51 +0900

    新規登録ページを調整

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,11 +3,16 @@ require "test_helper"
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test 'layout_links' do
+    # トップページの統合テスト
     get root_path
     assert_template 'public/tops/top'
     # root_pathが描画するテンプレートが'public/tops/top'になってるか確認。
     assert_select 'a[href=?]', about_path
     # 上記の書き方で、?の部分をabout_pathに置換する。
+
+    # ユーザー登録ページの統合テスト
+    get signup_path
+    assert_template 'public/users/new'
   end
 
 end
