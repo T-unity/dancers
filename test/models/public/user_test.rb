91dae6d5a5d5bb17307c4733a85e338685e2f64a
T-unity <takadanaritoshi@gmail.com>
2022-06-07 21:05:45 +0900

    Public::Userモデルのテストを追加

--- test/models/public/user_test.rb
+++ test/models/public/user_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class Public::UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = Public::User.new(name: 'T-unity', email: 'tea-uni@example.com')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
 end
