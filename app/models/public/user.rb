3dbd3947648789f781ae6c0771430ccf4ecd4332
T-unity <takadanaritoshi@gmail.com>
2022-06-11 13:23:33 +0900

    データを保存する前にemailを小文字に変換

--- app/models/public/user.rb
+++ app/models/public/user.rb
@@ -1,4 +1,6 @@
 class Public::User < ApplicationRecord
+  # before_save { self.email = self.email.downcase }
+  before_save { self.email = email.downcase }
   # presence: trueはハッシュ（連想配列）だが、メソッドの最後の引数としてハッシュを渡す場合は波括弧が不要
   # オプションハッシュ
   validates :name, presence: true, length: { maximum: 50 }
