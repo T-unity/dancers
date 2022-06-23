11cac4d4ba9b6784931639ff1b3ffd52c62a983a
T-unity <takadanaritoshi@gmail.com>
2022-06-23 21:46:34 +0900

    ユーザーの作成機能を追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,35 @@
 class Public::UsersController < ApplicationController
   def new
-    # @user = Public::User.new
+    @user = Public::User.new
+  end
+
+  def create
+    # @user = Public::User.new( params[:user] )
+    @user = Public::User.new( user_params )
+    if  @user.save
+      # 成功
+    else
+      exit
+    end
   end
 
   def show
     @user = Public::User.find(params[:id])
     debugger
   end
+
+  private
+
+    # ストロングパラメータ
+    def user_params
+      # requireとpermitに関しては以下を参照
+      # https://qiita.com/gogotakataka1234/items/47ad430fed87a2335f6d
+      params.require(:public_user).permit(
+        :name,
+        :email,
+        :password,
+        :password_confirmation,
+      )
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,12 @@ Rails.application.routes.draw do
 
   # ユーザー登録
   get :signup, to: 'public/users#new',  as: 'signup'
+  post :signup, to: 'public/users#create'
 
   # ユーザー
   scope module: :public do
-    resources :users, except: :new
+    # resources :users, except: :new
+    resources :users, except: [:new, :create]
     # resources :users
   end
 
