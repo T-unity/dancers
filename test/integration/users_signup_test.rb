7454d760fdc768aba3d0596f70bfdfee27fc9e5f
T-unity <takadanaritoshi@gmail.com>
2022-06-25 14:17:03 +0900

    ユーザー登録で無効な情報が送信された際に弾かれる事を確認するテストを追加

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,21 +1,21 @@
-# require "test_helper"
+require "test_helper"
 
-# class UsersSignupTest < ActionDispatch::IntegrationTest
-#   # test "the truth" do
-#   #   assert true
-#   # end
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
 
-#   test 'invalid signup' do
-#     get signup_path
-#     assert_no_difference 'Public::User.count' do
-#       post signup_path, params: { public_user: {
-#         name: '',
-#         email: 'hoge@ivalid',
-#         pasword: 'hoge',
-#         password_confirmation: 'fuga'
-#       } }
-#     end
-#     assert_template 'public/users/new'
-#   end
+  test 'invalid signup' do
+    get signup_path
+    assert_no_difference 'Public::User.count' do
+      post signup_path, params: { public_user: {
+        name: '',
+        email: 'hoge@ivalid',
+        pasword: 'hoge',
+        password_confirmation: 'fuga'
+      } }
+    end
+    assert_template 'public/users/new'
+  end
 
-# end
+end
