a2ce98c0a784da123384e267fc4aa5bedac19da2
T-unity <takadanaritoshi@gmail.com>
2022-06-27 15:28:28 +0900

    現在のログインユーザーの判定を追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,11 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def current_user
+    if session[:user_id]
+      # Public::User.find_by(id: session[:user_id])
+      @current_user ||= Public::User.find_by(id: session[:user_id])
+    end
+  end
+
 end
