4f38a9d972052cbb5d5757c074e787d9ccb9a30b
T-unity <takadanaritoshi@gmail.com>
2022-07-18 21:31:09 +0900

    WIP：永続セッション実装

--- app/models/public/user.rb
+++ app/models/public/user.rb
@@ -1,13 +1,18 @@
 class Public::User < ApplicationRecord
+  # 仮想の属性を設定
+  attr_accessor :remember_token
+
   # before_save { self.email = self.email.downcase }
   # before_save { self.email = email.downcase }
   before_save { email.downcase! }
+
   # presence: trueはハッシュ（連想配列）だが、メソッドの最後の引数としてハッシュを渡す場合は波括弧が不要
   # オプションハッシュ
   validates :name, presence: true, length: { maximum: 50 }
   # 以下と同義
   # validates(:name, presence: true)
   # validates(:name, {presence: true} )
+
   # 大文字で始まる文字列はRubyでは定数を示す。
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -23,8 +28,19 @@ class Public::User < ApplicationRecord
 
   # def Public::User.digest( string )
   # def User.digest( string )
-  #   cost = ActiveModel::has_secure_password.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-  #   BCrypt::Password.create(string, cost: cost)
-  # end
+  def digest( string )
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # def Public::User.new_token
+  def generate_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def set_remember_token
+    self.remember_token = generate_token
+    update_attribute(:remember_digest, digest(remember_token))
+  end
 
 end
