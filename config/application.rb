4386c7ee805d1f41fd4bd658d29ea2b5aad776fc
T-unity <takadanaritoshi@gmail.com>
2022-07-20 01:01:44 +0900

    view specを作成しないよう調整

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # rspecのセットアップ
+    config.generators do |g|
+      g.test_framework :rspec,
+      view_specs: false,
+    end
   end
 end
