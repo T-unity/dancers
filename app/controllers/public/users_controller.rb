782393bd8336fd47b65f106d72d0e764c9d69eea
T-unity <takadanaritoshi@gmail.com>
2022-07-21 00:05:41 +0900

    ユーザーのEdit画面を追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -4,27 +4,22 @@ class Public::UsersController < ApplicationController
   end
 
   def create
-    # @user = Public::User.new( params[:user] )
     @user = Public::User.new( user_params )
     if  @user.save
-      # 成功
       log_in @user
       flash[:success] = '登録に成功しました'
-      # railsチュートリアルでは以下でもOKという事になっているが、
-      # redirect_to @user
       redirect_to user_path( @user )
     else
-      # exit
-      # redirect_to signup_path
-      # 以下の記述で同一コントローラ内のnewアクションに対応するテンプレートを呼び出す
       render action: :new
-      # render template: ''
     end
   end
 
   def show
     @user = Public::User.find(params[:id])
-    # debugger
+  end
+
+  def edit
+    @user = Public::User.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  # namespace :public do
-  #   get 'users/new'
-  # end
-  # namespace :public do
-    # get 'tops#about'
-  # end
   root 'public/tops#top'
-  # 以下2つは同義
-  # get '/about', to: 'public/tops#about'
   get :about, to: 'public/tops#about', as: 'about'
 
   # ユーザー登録
@@ -16,9 +8,7 @@ Rails.application.routes.draw do
 
   # ユーザー
   scope module: :public do
-    # resources :users, except: :new
     resources :users, except: [:new, :create]
-    # resources :users
   end
 
   # セッション管理
