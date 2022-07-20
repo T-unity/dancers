277605bc1947fff87d6d635a5819a61ceb1aeb7d
T-unity <takadanaritoshi@gmail.com>
2022-07-21 01:34:11 +0900

    バリデーションを一旦無視して更新可能に変更

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -22,6 +22,17 @@ class Public::UsersController < ApplicationController
     @user = Public::User.find(params[:id])
   end
 
+  def update
+    @user = Public::User.find(params[:id])
+    # バリデーションを無視して直接カラムを更新しているので要検討
+    if @user.update_columns(name: user_update_params['name']) && @user.update_columns(email: user_update_params['email'])
+      flash[:success] = '登録情報を更新しました'
+      redirect_to user_path( @user )
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     # ストロングパラメータ
@@ -33,6 +44,13 @@ class Public::UsersController < ApplicationController
         :email,
         :password,
         :password_confirmation,
+      )
+    end
+
+    def user_update_params
+      params.require(:public_user).permit(
+        :name,
+        :email,
       )
     end
 
